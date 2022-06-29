7ed624a50857a81629a0d5ea02a139bb122c1740
yannickfrd <farade.y@hotmail.fr>
2022-06-29 15:57:55 +0200

    feat: install bootstrap

---
+++ assets/app.js
@@ -0,0 +1,6 @@
+// any CSS you import will output into a single css file (app.css in this case)
+import './styles/app.scss';
+
+// start the Stimulus application
+import './bootstrap';
+import bootstrap from 'bootstrap'
